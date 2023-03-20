b2d70df1e841a3bc51a74476d5a3b7c3035cb703
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-03-20 23:54:10 +0530

    add requests html web scraping

--- questionsCrawler.py
+++ questionsCrawler.py
@@ -1,62 +1,116 @@
 import json
-import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
-with open('quizData.json') as f:
-    quizData = json.load(f)
+session = HTMLSession()
+
+# nda questions website url
+req = session.get('https://www.4ono.com/nda-english-test/')
+
+spottingErrSelector = 'body > div.box.article_page > div.o_center > article > div > ol:nth-child(5) > li:nth-child(2)'
+antonymsSelector = 'body > div.box.article_page > div.o_center > article > div > ol:nth-child(6)'
+synonymsSelector = 'body > div.box.article_page > div.o_center > article > div > ol:nth-child(7)'
+fillBlanksSelector = 'body > div.box.article_page > div.o_center > article > div > ol:nth-child(8)'
+sentenceImprovementSelector = 'body > div.box.article_page > div.o_center > article > div > ol:nth-child(9)'
+sequenceSelector = 'body > div.box.article_page > div.o_center > article > div > ol:nth-child(10)'
+
+reqQuestionsOptions = req.html.find(sequenceSelector, first=True)
+
+questionsList = reqQuestionsOptions.find('li > p')
+options = reqQuestionsOptions.find('li > ol')
 
 
-url = 'https://www.4ono.com/nda-english-test/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-questions = soup.find_all('div', class_='entry-content')
-for question in questions:
-    question_text = question.find('h3').text
-    options = question.find_all('li')
-    for option in options:
-        option_text = option.text
-        print(option_text)
+def correctOpt(str):
+    if str in optionsList[4].text:
+        return True
+    return False
 
 
-# set the data to json string
-optionsData = [
-    {
-        "id": 1,
-        "label": "A",
-        "text": "",
-        "isCorrect": False
-    },
-    {
-        "id": 2,
-        "label": "B",
-        "text": "",
-        "isCorrect": False
-    },
-    {
-        "id": 3,
-        "label": "C",
-        "text": "",
-        "isCorrect": False
-    },
-    {
-        "id": 4,
-        "label": "D",
-        "text": "",
-        "isCorrect": False
+QuestionsList = []
+# def getQuestionsAnswers(selector):
+for index, question in enumerate(questionsList):
+    optionsList = options[index].find('li')
+    optionsData = [
+        {
+            "id": 1,
+            "label": "A",
+            "text": optionsList[0].text,
+            "isCorrect": correctOpt(optionsList[0].text)
+        },
+        {
+            "id": 2,
+            "label": "B",
+            "text": optionsList[1].text,
+            "isCorrect": correctOpt(optionsList[1].text)
+        },
+        {
+            "id": 3,
+            "label": "C",
+            "text": optionsList[2].text,
+            "isCorrect": correctOpt(optionsList[2].text)
+        },
+        {
+            "id": 4,
+            "label": "D",
+            "text": optionsList[3].text,
+            "isCorrect": correctOpt(optionsList[3].text)
+        }
+    ]
+    questionsData = {
+        "id": index+1,
+        "text": question.text,
+        "options": optionsData
     }
-]
 
-questionsData = [
-    {id: 1,
-     "text": "",
-     "options": optionsData}
-]
+    QuestionsList.append(questionsData)
 
-# print(type(quizData))
-# print(json.dumps(optionsData, indent=2))
-# print(optionsData[3]['id'])
-# print(json.dumps(quizData, indent=2))
+# print( len(QuestionsList))
 
+# questionsJsonData = json.dumps(QuestionsList)
+# print(questionsJsonData)
 
-# print(json.dumps(quizData[0].questions, indent=2))
+# write data to json format
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(QuestionsList, f, ensure_ascii=False, indent=2)
+
+'''
+1. Get questions one by one
+2. Set OptionsList to same index as question
+3. Set options from one to four
+4. Use Options Object to set optionsData
+5. Use Question dict to set Question and optionsData
+6. Push the Question to the topic Array
+'''
+
+# Options Data BluePrint || set the data to json string
+# optionsData = [
+#     {
+#         "id": 1,
+#         "label": "A",
+#         "text": "",
+#         "isCorrect": False
+#     },
+#     {
+#         "id": 2,
+#         "label": "B",
+#         "text": "",
+#         "isCorrect": False
+#     },
+#     {
+#         "id": 3,
+#         "label": "C",
+#         "text": "",
+#         "isCorrect": False
+#     },
+#     {
+#         "id": 4,
+#         "label": "D",
+#         "text": "",
+#         "isCorrect": False
+#     }
+# ]
+
+# Question Data BluePrint
+# questionsData = {"id": 1,
+#                  "text": "",
+#                  "options": optionsData}
